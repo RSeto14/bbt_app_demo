de85d4c86fc2102024dee52cb11e54b29b3b91b3
RSeto14 <107923048+RSeto14@users.noreply.github.com>
2023-04-09 18:01:00 +0900

    Update demo_bbt_analysis_app.py

--- demo_bbt_analysis_app.py
+++ demo_bbt_analysis_app.py
@@ -8,6 +8,7 @@ from plotly.subplots import make_subplots
 import numpy as np
 from io import BytesIO
 
+st.write("demo用")
 # スプレッドシートの認証
 scopes = [ 'https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'
 ]
